70e8fe1e885352fbfcf0322212edcdf36fb8e5ab
Putra Fajar <putrautalls@gmail.com>
2022-07-26 12:01:09 +0700

    create func getCustomer to get data By ID

--- app/app.go
+++ app/app.go
@@ -1,12 +1,17 @@
 package app
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func Start() {
-	mux := http.NewServeMux()
+	mux := mux.NewRouter()
 	// * defining routes
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getCustomers)
+	mux.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	mux.HandleFunc("/customers", getCustomers).Methods(http.MethodGet)
+	mux.HandleFunc("/customers/{customer_id}", getCustomer).Methods(http.MethodGet)
 
 	// * starting the server
 	http.ListenAndServe(":8080", mux)
--- app/handler.go
+++ app/handler.go
@@ -5,19 +5,23 @@ import (
 	"encoding/xml"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Customer struct {
 	// untuk merubah key tampilan di json maupun xml dari prefix huruf besar menjadi huruf kecil
+	ID      int    `json:"id" xml:"id"`
 	Name    string `json:"name" xml:"name"`
 	City    string `json:"city" xml:"city"`
 	ZipCode string `json:"zip_code" xml:"zipcode"`
 }
 
 var customers []Customer = []Customer{
-	{"User1", "Jakarta", "12345"},
-	{"User2", "Surabaya", "67890"},
-	{"User3", "Semarang", "76584"},
+	{1, "User1", "Jakarta", "12345"},
+	{2, "User2", "Surabaya", "67890"},
+	{3, "User3", "Semarang", "76584"},
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
@@ -38,4 +42,26 @@ func getCustomers(w http.ResponseWriter, r *http.Request) {
 		// Marshaling data structure to JSON Representation
 		json.NewEncoder(w).Encode(customers)
 	}
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	// Untuk mengambil ID, get route variable
+	vars := mux.Vars(r)
+	customerId := vars["customer_id"]
+
+	// Convert string to int
+	id, _ := strconv.Atoi(customerId)
+
+	// Searching customer data
+	var cust Customer
+
+	for _, data := range customers {
+		if data.ID == id {
+			cust = data
+		}
+	}
+
+	// Return customer data
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(cust)
 }
