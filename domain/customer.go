1547a03a6c45ba195266c728f975071e29a8980d
Putra Fajar <putrautalls@gmail.com>
2022-07-28 14:28:06 +0700

    install sqlx and apply it in customerRepositoryDB.go to connect database

--- domain/customer.go
+++ domain/customer.go
@@ -4,11 +4,11 @@ import "capi/errs"
 
 type Customer struct {
 	// untuk merubah key tampilan di json maupun xml dari prefix huruf besar menjadi huruf kecil
-	ID          string `json:"id" xml:"id"`
+	ID          string `json:"id" xml:"id" db:"customer_id"`
 	Name        string `json:"name" xml:"name"`
 	City        string `json:"city" xml:"city"`
 	ZipCode     string `json:"zip_code" xml:"zipcode"`
-	DateOfBirth string `json:"date_of_birth" xml:"dateofbirth"`
+	DateOfBirth string `json:"date_of_birth" xml:"dateofbirth" db:"date_of_birth"`
 	Status      string `json:"status" xml:"status"`
 }
 
--- domain/customerRepositoryDB.go
+++ domain/customerRepositoryDB.go
@@ -6,18 +6,19 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
 type CustomerRepositoryDB struct {
-	client *sql.DB
+	client *sqlx.DB
 }
 
 // Create constructor
 func NewCustomerRepositoryDB() CustomerRepositoryDB {
 	// In development mode postgreSQL, sslmode setting to disable instead of verify-full
 	connStr := "postgres://postgres:ktl123@localhost/banking_cap?sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+	db, err := sqlx.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,10 +28,11 @@ func NewCustomerRepositoryDB() CustomerRepositoryDB {
 func (d CustomerRepositoryDB) FindByID(customerID string) (*Customer, *errs.AppErr) {
 	query := "select * from customers where customer_id = $1"
 
-	row := d.client.QueryRow(query, customerID)
+	// row := d.client.QueryRow(query, customerID)
 
 	var c Customer
-	err := row.Scan(&c.ID, &c.Name, &c.DateOfBirth, &c.City, &c.ZipCode, &c.Status)
+	err := d.client.Get(&c, query, customerID)
+	// err := row.Scan(&c.ID, &c.Name, &c.DateOfBirth, &c.City, &c.ZipCode, &c.Status)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			logger.Error("error customer data not found" + err.Error())
