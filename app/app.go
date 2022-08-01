34648f4150d998724ba674f04deb58c28d15351a
Putra Fajar <putrautalls@gmail.com>
2022-08-01 11:59:54 +0700

    added environmental variable to the project in app.js

--- app/app.go
+++ app/app.go
@@ -2,13 +2,38 @@ package app
 
 import (
 	"capi/domain"
+	"capi/logger"
 	"capi/service"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
+func sanityCheck() {
+	envProps := []string{
+		"SERVER_ADDRESS",
+		"SERVER_PORT",
+	}
+
+	for _, envKey := range envProps {
+		if os.Getenv(envKey) == "" {
+			logger.Fatal(fmt.Sprintf("environment variable %s not defined, terminating application..", envKey))
+		}
+	}
+	logger.Info("environment variable loaded...")
+}
+
 func Start() {
+	err := godotenv.Load()
+	if err != nil {
+		logger.Fatal("Error loading .env file")
+	}
+
+	sanityCheck()
+
 	// * wiring
 	ch := CustomerHandler{service.NewCustomerService(domain.NewCustomerRepositoryDB())}
 
@@ -23,6 +48,10 @@ func Start() {
 	// mux.HandleFunc("/customers/{customer_id:[0-9]+}", updatedCustomer).Methods(http.MethodPut)
 	// mux.HandleFunc("/customers/{customer_id:[0-9]+}", deleteCustomer).Methods(http.MethodDelete)
 
-	// * starting the server
-	http.ListenAndServe(":8080", mux)
+	// * starting the server and set environment variable
+	serverAddr := os.Getenv("SERVER_ADRESS")
+	serverPort := os.Getenv("SERVER_PORT")
+
+	logger.Info(fmt.Sprintf("start server on %s:%s ...", serverAddr, serverPort))
+	http.ListenAndServe(fmt.Sprintf("%s:%s", serverAddr, serverPort), mux)
 }
--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"capi/app"
 	"capi/logger"
+	"fmt"
+	"os"
 )
 
 func main() {
+	// Environment Variable
+	fmt.Println(os.Getenv("TEST"))
 	logger.Info("starting application...")
 	app.Start()
 }
