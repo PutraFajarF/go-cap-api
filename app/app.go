0c351cb2210f01524ec2294a0d97a9925e6d6508
Putra Fajar <putrautalls@gmail.com>
2022-08-01 15:32:27 +0700

    main.go updated

--- app/app.go
+++ app/app.go
@@ -1,13 +1,100 @@
 package app
 
-import "net/http"
+import (
+	"capi/domain"
+	"capi/logger"
+	"capi/service"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+)
+
+func sanityCheck() {
+	envProps := []string{
+		"SERVER_ADDRESS",
+		"SERVER_PORT",
+		"DB_USER",
+		"DB_PASSWD",
+		"DB_ADDR",
+		"DB_PORT",
+		"DB_NAME",
+	}
+
+	for _, envKey := range envProps {
+		if os.Getenv(envKey) == "" {
+			logger.Fatal(fmt.Sprintf("environment variable %s not defined. terminating application...", envKey))
+		}
+	}
+
+	logger.Info("environment variables loaded...")
+
+}
 
 func Start() {
 
+	err := godotenv.Load()
+	if err != nil {
+		logger.Fatal("error loading .env file")
+	}
+	logger.Info("load environment variables...")
+
+	sanityCheck()
+
+	dbClient := getClientDB()
+
+	// * wiring
+	// * setup repository
+	customerRepositoryDB := domain.NewCustomerRepositoryDB(dbClient)
+	accountRepositoryDB := domain.NewAccountRepositoryDB(dbClient)
+	authRepositoryDB := domain.NewAuthRepositoryDB(dbClient)
+
+	// * setup service
+	customerService := service.NewCustomerService(customerRepositoryDB)
+	accountService := service.NewAccountService(accountRepositoryDB)
+	authService := service.NewAuthService(authRepositoryDB)
+
+	// * setup handler
+	ch := CustomerHandlers{customerService}
+	ah := AccountHandler{accountService}
+	authH := AuthHandler{authService}
+
+	// * create ServeMux
+	mux := mux.NewRouter()
+
 	// * defining routes
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getCustomers)
+	mux.HandleFunc("/auth/login", authH.Login).Methods(http.MethodPost)
+
+	mux.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
+	mux.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomerByID).Methods(http.MethodGet)
+	mux.HandleFunc("/customers/{customer_id:[0-9]+}/accounts", ah.NewAccount).Methods(http.MethodPost)
+	mux.HandleFunc("/customers/{customer_id:[0-9]+}/accounts/{account_id:[0-9]+}", ah.MakeTransaction).Methods(http.MethodPost)
 
 	// * starting the server
-	http.ListenAndServe(":8080", nil)
+
+	serverAddr := os.Getenv("SERVER_ADDRESS")
+	serverPort := os.Getenv("SERVER_PORT")
+
+	logger.Info(fmt.Sprintf("start server on %s:%s...", serverAddr, serverPort))
+	http.ListenAndServe(fmt.Sprintf("%s:%s", serverAddr, serverPort), mux)
+}
+
+func getClientDB() *sqlx.DB {
+	dbUser := os.Getenv("DB_USER")
+	dbPasswd := os.Getenv("DB_PASSWD")
+	dbAddr := os.Getenv("DB_ADDRESS")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPasswd, dbAddr, dbPort, dbName)
+	db, err := sqlx.Open("postgres", connStr)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	logger.Info("success connect to database...")
+
+	return db
 }
